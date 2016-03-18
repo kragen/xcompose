985c78396272e460deb52c8729bd57a5706e404e
Mark Shoulson <mark@kli.org>
2016-03-17 21:21:56 -0400

    Add some missed emoji, add conventions to interim, add sorter, add emoji.compose, even though it's derived.

--- emojitrans2.pl
+++ emojitrans2.pl
@@ -35,6 +35,11 @@ sub splitup {
 }
 
 unless (/^#/) {
+    my $hold=$_;
     s/<MM>/<Multi_key> <Multi_key>/;
     s({([][[:alnum:] _+%@><,.^\$+#()!/-]+)})(splitup($1))e;
+   if (length($1) > 7) {
+       $_=$hold;
+       s/^<MM>/### <MM>/;
+   }
 }
---
+++ sort-interim.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+
+my $this;
+my %elts;
+
+sub process {
+    my $data = shift;
+    my $key;
+
+    $data =~ m@\s+(U[[:xdigit:]]+)\s+@;
+    $key=$1;
+    if ($elts{$key}) {
+	print STDERR "Warning: key $key found more than once.\n";
+    }
+    $elts{$key}=$data;
+}
+
+while (<>) {
+    if (/(?:###\+)?<MM>/) {
+	while (/(?:###\+)?<MM>/) {
+	    $this .= $_;
+	    $_ = <>;
+	}
+	process $this;
+	$this='';
+    }
+    $this .= $_;
+}
+
+for $k (sort keys %elts) {
+    print $elts{$k};
+}
